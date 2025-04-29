34cf095e6af9c68942f332ace8ce0fd02ced8d8b
Ericoss92 <eric.e.succar@gmail.com>
2025-04-29 15:40:40 +0200

    Adding a footer button 'Knefeh' for a new page

--- app/src/main/java/com/example/myfirstapp/MainScreen.kt
+++ app/src/main/java/com/example/myfirstapp/MainScreen.kt
@@ -1,10 +1,13 @@
 package com.example.myfirstapp
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,7 +23,23 @@ fun MainScreen(
     modifier: Modifier = Modifier,
 ) {
     val itemsList = List(100) { "Knefeh $it" }
-    Scaffold(modifier = modifier) { innerPadding ->
+    Scaffold(
+        modifier = modifier,
+        bottomBar = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.Bottom,
+            ) {
+                Button(
+                    onClick = { },
+                    content = { Text("Knefeh") },
+                )
+            }
+        },
+    ) { innerPadding ->
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
@@ -42,4 +61,3 @@ fun GreetingPreview() {
         MainScreen()
     }
 }
-
